9fc7c3a1110b875dea0b2b07f002ace344a1fd72
Mohammed Benkhattab <mbenkhat@e2r6p1.1337.ma>
2022-02-08 13:58:14 +0100

    1

--- philo.c
+++ philo.c
@@ -6,32 +6,58 @@
 /*   By: mbenkhat <mbenkhat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 15:36:32 by mbenkhat          #+#    #+#             */
-/*   Updated: 2022/02/03 19:38:39 by mbenkhat         ###   ########.fr       */
+/*   Updated: 2022/02/04 20:16:44 by mbenkhat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*handler(void *params);
+void	eating(t_data	*params)
+{
+	pthread_mutex_lock(params->philo->mutex);
+	printf("%dms %d has taken a fork", params->time, params->philo->nbr);
+	printf("%dms %d has taken a fork", params->time, params->philo->nbr);
+	printf("%dms %d is eating", params->time, params->philo->nbr);
+	usleep(params->time_to_eat * 1000);
+	params->time += params->time_to_eat;
+	pthread_mutex_unlock(params->philo->mutex);
+}
 
-philo_t	*create_philos(t_data *args)
+void	sleeping(t_data	*params)
+{
+	pthread_mutex_lock(params->philo->mutex);
+	printf("%dms %d is sleeping", params->time, params->philo->nbr);
+	usleep(params->time_to_sleep * 1000);
+	params->time += params->time_to_sleep;
+	pthread_mutex_unlock(params->philo->mutex);
+}
+
+void	*philo_handler(t_data *params)
+{
+	if (params->philo->nbr % 2 == 0)
+	{
+		eating(params);
+		sleeping(params);
+	}
+	else
+		printf("%dms %d is thinking", params->time, params->philo->nbr);
+	
+}
+
+t_philo	*create_philos(t_data *args)
 {
 	int	i;
-	philo_t	*philos;
+	t_philo	*philos;
 	
-	philos = malloc(sizeof(philo_t) * args->n_of_philos);
+	philos = malloc(sizeof(t_philo) * args->n_of_philos);
 	i = 1;
 	while (i <= args->n_of_philos)
 	{
-		philos->nbr = i;
-		pthread_create(philos->thread, NULL, &handler, args);
+		(&philos[i - 1])->nbr = i++;
+		if (pthread_create(philos->thread, NULL, &philo_handler, args))
+			return (0);
 	}
 	return (philos);
-}
-
-int	philo(t_data *args)
-{
-	
 }
 
 int	main(int ac, char **av)
--- philo.h
+++ philo.h
@@ -6,17 +6,24 @@
 /*   By: mbenkhat <mbenkhat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 15:36:43 by mbenkhat          #+#    #+#             */
-/*   Updated: 2022/02/03 19:33:06 by mbenkhat         ###   ########.fr       */
+/*   Updated: 2022/02/04 11:57:47 by mbenkhat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
+#ifndef PHILO_H
+# define PHILO_H
 
 # include <pthread.h>
 # include <unistd.h>
 # include <stdio.h>
 # include <string.h>
+
+typedef struct s_philo
+{
+	int			nbr;
+	pthread_t	*thread;
+	pthread_mutex_t	*mutex;
+}			t_philo;
 
 typedef struct s_data
 {
@@ -26,13 +33,8 @@ typedef struct s_data
 	int time_to_sleep;
 	int n_of_times_to_eat;
 	int	time;
+	t_philo	*philo;
 }			t_data;
-
-typedef struct philo_s
-{
-	int			nbr;
-	pthread_t	*thread;
-}			philo_t;
 
 int	check_params(int ac, char **av);
 
